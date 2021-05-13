bbc50cae60445ee190d6e92c9ee464d66163d9e9
dephit <sergeenko123@gmail.com>
2021-05-13 09:30:51 +0300

    commit2

---
+++ mylibrary/src/main/java/com/sergeenko/mylibrary/MyFirebaseMessagingService.kt
@@ -0,0 +1,30 @@
+package com.sergeenko.mylibrary;
+
+import android.app.Service;
+import android.util.Log
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage
+import com.sergeenko.mylibrary.LibraryInstance.TAG
+
+public class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+    }
+}
